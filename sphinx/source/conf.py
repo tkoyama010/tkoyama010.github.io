5ce21653811aac2d6e0c45e68a2eb0bd419bbfcc
Tetsuo Koyama <tkoyama010@gmail.com>
2018-06-27 11:45:03 +0900

    build

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import shlex
-import sphinx-rtd-theme
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
